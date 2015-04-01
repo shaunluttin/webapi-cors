591b5cc1797a81a55060a9b14bcfa6356df9fab3
unknown <Shaun.Luttin@CANL-JBWNK32.gds.ca>
2015-04-01 10:32:26 -0700

    Initial commit.

---
+++ CORS/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CORS
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            EnableCrossSiteRequests(config);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        private static void EnableCrossSiteRequests(HttpConfiguration config)
+        {
+#if DEBUG
+            var cors = new EnableCorsAttribute(origins: "*", headers: "*", methods: "*");
+#else
+            var cors = new EnableCorsAttribute("http://attendance1.azurewebsites.net", headers: "*", methods: "*");
+#endif
+            config.EnableCors(cors);
+        }
+    }
+}
---
+++ CORS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CORS.WebApiApplication" Language="C#" %>

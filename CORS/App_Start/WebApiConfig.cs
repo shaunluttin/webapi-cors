6f538a584b5210a6720d7edd38418aa2f409873c
unknown <Shaun.Luttin@CANL-JBWNK32.gds.ca>
2015-04-01 10:59:24 -0700

    Remove cruft.

--- CORS/App_Start/WebApiConfig.cs
+++ CORS/App_Start/WebApiConfig.cs
@@ -10,11 +10,21 @@ namespace CORS
     {
         public static void Register(HttpConfiguration config)
         {
-            var cors = new EnableCorsAttribute(
-                origins: "*", 
-                headers: "*", 
-                methods: "*");
+            EnableCrossSiteRequests(config);
+            AddRoutes(config);
+        }
 
+        private static void AddRoutes(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "Default",
+                routeTemplate: "api/{controller}/"
+            );
+        }
+
+        private static void EnableCrossSiteRequests(HttpConfiguration config)
+        {
+            var cors = new EnableCorsAttribute(origins: "*", headers: "*", methods: "*");
             config.EnableCors(cors);
         }
     }

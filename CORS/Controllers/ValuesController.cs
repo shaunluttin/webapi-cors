60ef944eb125faab0edb7825fe3e82423c3f726a
unknown <Shaun.Luttin@CANL-JBWNK32.gds.ca>
2015-04-01 10:50:09 -0700

    Remove cruft.

--- CORS/Controllers/ValuesController.cs
+++ CORS/Controllers/ValuesController.cs
@@ -12,13 +12,7 @@ namespace CORS.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
+            return new string[] { "This is a CORS request.", "That works from any origin." };
         }
     }
 }

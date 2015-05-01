949b53d10729c4819cec7ee3ee9e6a0b13dd3323
Shaun Luttin <shaunluttin@gmail.com>
2015-04-30 19:23:59 -0700

    Update TitleEstimateController.cs

--- CORS/Controllers/TitleEstimateController.cs
+++ CORS/Controllers/TitleEstimateController.cs
@@ -1,13 +1,28 @@
-public class TitleEstimateController : ApiController
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CORS.Controllers
 {
-    public IHttpActionResult GetTitleEstimate([FromUri] EstimateQuery query)
+    public class TitleEstimateController : ApiController
     {
+        public class EstimateQuery
+        {
+            public string username { get; set; }
+        }
+        
+        public IHttpActionResult GetTitleEstimate([FromUri] EstimateQuery query)
+        {
             // All the values in "query" are null or zero
             // Do some stuff with query if there were anything to do
+            if(query != null && query.username != null)
+            {
+                return Ok(query.username);
+            }
+            else
+            {
+                return Ok("Add a username!");
+            }
+        }        
     }
-}
-
-public class EstimateQuery
-{
-    // Various fields
 }

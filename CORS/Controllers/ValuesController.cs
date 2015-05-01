8e0d36b2fe2cb1356547fc48b3063f88c3b069da
Shaun Luttin <shaunluttin@gmail.com>
2015-04-30 19:11:10 -0700

    Return the passed query.

--- CORS/Controllers/ValuesController.cs
+++ CORS/Controllers/ValuesController.cs
@@ -29,8 +29,14 @@ namespace CORS.Controllers
         {
             // All the values in "query" are null or zero
             // Do some stuff with query if there were anything to do
-            
-            return Ok("Hello world.");
+            if(query != null)
+            {
+                return Ok(query.username);
+            }
+            else
+            {
+                return Ok("Add a username!")
+            }
         }        
     }
 }

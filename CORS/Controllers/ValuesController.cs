27c7409407ff80d84aaeaa45c083ff2d23f13417
Shaun Luttin <shaunluttin@gmail.com>
2015-04-30 19:24:19 -0700

    Update ValuesController.cs

--- CORS/Controllers/ValuesController.cs
+++ CORS/Controllers/ValuesController.cs
@@ -19,24 +19,5 @@ namespace CORS.Controllers
         {
             return new string[] { "This is a CORS request.", "It works only from www.bigfont.ca." };
         }
-        
-        public class EstimateQuery
-        {
-            public string username { get; set; }
-        }
-        
-        public IHttpActionResult GetTitleEstimate([FromUri] EstimateQuery query)
-        {
-            // All the values in "query" are null or zero
-            // Do some stuff with query if there were anything to do
-            if(query != null && query.username != null)
-            {
-                return Ok(query.username);
-            }
-            else
-            {
-                return Ok("Add a username!");
-            }
-        }        
     }
 }
